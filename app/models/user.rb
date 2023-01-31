05aa2d0c4fe4208b027d778d57ece2a007cce635
Manzar <manzar.hussain@telus.com>
2023-01-31 02:04:07 -0500

    Model Spec: User completed. Implemented test for authentication & Edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
-
 class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: { minimum: 8 }, allow_nil: true
-
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
+require 'rails_helper'
+
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
+    before(:each) do
+      @user = User.new(
+        first_name: "Manzar",
+        last_name: "Hussain",
+        email: "example@domain.com",
+        password: "testing12",
+        password_confirmation: "testing12",
+      )
+    end
+
     it "should have a password and password_confirmation fields" do
       user = User.new(email: "test@example.com", first_name: "John", last_name: "Doe")
       expect(user).to_not be_valid
@@ -18,14 +30,72 @@ RSpec.describe User, type: :model do
 
     it "emails should be unique (not case sensitive)" do
       user1 = User.create(email: "test@example.com", first_name: "John", last_name: "Doe", password: "password", password_confirmation: "password")
-      user2 = User.new(email: "Test@example.com", first_name: "Jane", last_name: "Doe", password: "password", password_confirmation: "password")
+      user2 = User.new(email: "test@example.com", first_name: "Jane", last_name: "Doe", password: "password", password_confirmation: "password")
       expect(user2).to_not be_valid
     end
-    
+
     it "password should have a minimum length of 8 characters" do
       user = User.new(email: "test@example.com", first_name: "John", last_name: "Doe", password: "pass", password_confirmation: "pass")
       expect(user).to_not be_valid
-    end   
+    end
+
+    it "should create a new user when all input is filled out" do
+      expect(@user).to be_valid
+      expect(@user.errors.full_messages).to be_empty
+    end
+    
+    it "should not create a new user when no password confirmation is provided" do
+      @user.password_confirmation = nil
+      expect(@user).not_to be_valid
+    end
+    
+    it "should not create a new user when no password is provided" do
+      @user.password = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    
+    it "should not create a new user when no password confirmation is provided" do
+      @user.password_confirmation = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+    
+    it "should give an error when no email is provided" do
+      @user.email = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    
+    it "should give an error when no first name is provided" do
+      @user.first_name = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+    
+    it "should give an error when no last name is provided" do
+      @user.last_name = nil
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+    
+    it "should give an error when a an email is already in database" do
+      @user.save
+      @user2 = User.new(
+        first_name: "Manzar",
+        last_name: "Hussain",
+        email: "example@domain.com",
+        password: "testing",
+        password_confirmation: "testing",
+      )
+      expect(@user2).not_to be_valid
+    end
+    
+    it "should give an error when password length is less than 8 characters" do
+      @user.password = "testing"
+      @user.password_confirmation = "testing"
+      expect(@user).not_to be_valid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
+    end    
   end
 end
-
